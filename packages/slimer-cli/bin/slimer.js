4f46a6b219692f16f69f47ed613614570e871924
Hannah Wolfe <erisds@gmail.com>
2018-09-21 13:58:47 +0100

    ✨ Added wiring for loading commands
    - Slimer CLI now knows how to load commands from slimer into the CLI
    - Slimer CLI is purposefully a minimal wrapper
    - This makes for a flexible, extensible structure

--- packages/slimer-cli/bin/slimer.js
+++ packages/slimer-cli/bin/slimer.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 const prettyCLI = require('@tryghost/pretty-cli');
+const slimer = require('@tryghost/slimer');
+
+slimer.commands.forEach((command) => {
+    prettyCLI.command(command);
+});
 
 prettyCLI
     .parseAndExit();
