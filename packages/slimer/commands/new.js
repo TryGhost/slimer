b41faac87d038108d955578507b4886290bfdfb7
Hannah Wolfe <erisds@gmail.com>
2018-09-21 14:11:33 +0100

    🎨 Refactored slimer into a sensible structure
    - We're going to have multiple commands, keep them separated in clear files
    - Ensure they get correctly exported

---
+++ packages/slimer/commands/new.js
@@ -0,0 +1,22 @@
+const ui = require('../ui');
+
+// Internal ID in case we need one.
+exports.id = 'new';
+
+// The command to run and any params
+exports.flags = 'new <name>';
+
+// Aliases, other names for the command
+exports.aliases = ['create', 'project'];
+
+// Description for the top level command
+exports.desc = 'Start a new project';
+
+// Descriptions for the individual params
+exports.paramsDesc = ['Name for the project folder'];
+
+// What to do when this command is executed
+exports.run = (argv) => {
+    ui.log('Will evenually run yeoman to create', argv.name);
+};
+
---
+++ packages/slimer/ui/index.js
@@ -0,0 +1,2 @@
+// @TODO use chalk / make this prettier
+module.exports.log = (...args) => console.log(...args); // eslint-disable-line no-console
