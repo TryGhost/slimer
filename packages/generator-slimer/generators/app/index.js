5049702df0051eef3a8483d1286c4eff4a399443
Hannah Wolfe <erisds@gmail.com>
2018-09-21 09:22:47 +0100

    ✨ Slimer Generator initial commit
    - added intial generator package
    - setup base generator class with some shared logic which I know is useful
    - ready to start getting it to DO THINGS.

---
+++ packages/generator-slimer/generators/app/index.js
@@ -0,0 +1,44 @@
+'use strict';
+const Generator = require('../../lib/Generator');
+
+// These are the options that can be passed in as flags e.g. --foo=bar
+const knownOptions = {};
+
+// These are arguments that are passed directly
+const knownArguments = {
+    name: {
+        type: String,
+        required: true,
+        desc: 'Project name'
+    }
+};
+
+module.exports = class extends Generator {
+    constructor(args, options) {
+        super(args, options, knownArguments, knownOptions);
+    }
+
+    initializing() {
+        super.initializing();
+
+        this.log('Initialized');
+        this.log(this.props);
+    }
+
+    prompting() {
+        this.log(`Welcome!`);
+
+        const prompts = [
+            {
+                type: 'confirm',
+                name: 'someAnswer',
+                message: 'Would you like to enable this option?',
+                default: true
+            }
+        ];
+
+        return this.prompt(prompts).then((answer) => {
+            this._mergeAnswers(answer);
+        });
+    }
+};
---
+++ packages/generator-slimer/lib/Generator.js
@@ -0,0 +1,39 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const _ = require('lodash');
+
+/**
+ * Custom Generator
+ *
+ * We setup some defaults to share between all of our Generators
+ */
+
+module.exports = class extends Generator {
+    constructor(args, options, knownArguments, knownOptions) {
+        super(args, options);
+
+        this.knownArguments = knownArguments;
+        this.knownOptions = knownOptions;
+
+        // Initialise our options & arguments
+        _.each(this.knownArguments, (value, key) => this.argument(key, value));
+        _.each(this.knownOptions, (value, key) => this.option(key, value));
+    }
+
+    _initProps() {
+        // Props is a store of config generated through options + prompting
+        // We intialise it by grabbing the allowed values passed in as options & arguments
+        this.props = _.pick(this.options, _.concat(_.keys(this.knownOptions),_.keys(this.knownArguments)));
+    }
+
+    // Initialize props by default
+    initializing() {
+        this._initProps();
+    }
+
+    _mergeAnswers(answers) {
+        // Merge answers with existing props
+        // To access props later use this.props.someAnswer;
+        _.extend(this.props, answers);
+    }
+};
