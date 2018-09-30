94d0678331c7f03a45320f4f203865ea4a65baaa
Hannah Wolfe <erisds@gmail.com>
2018-09-30 22:59:25 +0100

    ✨ Added package.json generation
    - This file is super important!
    - We need to be able to add linting, testing and shipping stuff to it

--- packages/generator-slimer/generators/node/index.js
+++ packages/generator-slimer/generators/node/index.js
@@ -1,11 +1,36 @@
 'use strict';
 const Generator = require('../../lib/Generator');
+const _ = require('lodash');
 
 const knownOptions = {
     type: {
         type: String,
         required: true,
         desc: 'What kind of project to create: [module, app, pkg, mono]'
+    },
+    public: {
+        type: Boolean,
+        desc: 'Is the project public?'
+    },
+    org: {
+        type: String,
+        default: 'TryGhost',
+        desc: 'GitHub Organisation'
+    },
+    npmName: {
+        type: String,
+        required: true,
+        desc: 'The npm package name for the project'
+    },
+    repoName: {
+        type: String,
+        required: true,
+        desc: 'The GitHub repository name for the project'
+    },
+    repo: {
+        type: String,
+        desc: 'The URL of the GitHub repository',
+        hidden: true
     }
 };
 
@@ -18,6 +43,8 @@ module.exports = class extends Generator {
 
     initializing() {
         super.initializing();
+
+        this.props.main = this.props.type === 'app' ? 'app.js' : 'index.js';
     }
 
     prompting() {
@@ -36,16 +63,36 @@ module.exports = class extends Generator {
     }
 
     _writeMainFile() {
-        let main = this.props.type === 'app' ? 'app.js' : 'index.js';
-
         this.fs.copyTpl(
             this.templatePath('blank.js'),
-            this.destinationPath(main)
+            this.destinationPath(this.props.main)
+        );
+    }
+
+    // Add package.json file (super important!)
+    _writePackageJson() {
+        let repo = this.props.type === 'pkg' ?
+            `${this.props.repo}/tree/master/packages/${this.props.repoName}` :
+            `git@github.com:${this.props.org}/${this.props.repoName}.git`;
+
+        this.fs.copyTpl(
+            this.templatePath('package.json'),
+            this.destinationPath('package.json'),
+            {
+                npmName: this.props.npmName,
+                license: this.props.public ? '"license": "MIT",' : '"private": true,',
+                publicScoped: this.props.public && _.startsWith(this.props.packageName, '@'),
+                repo: repo,
+                main: this.props.main
+            }
         );
     }
 
     default() {
         // Create a "main" JS file
         this._writeMainFile();
+
+        // Create a package.json file
+        this._writePackageJson();
     }
 };
