aa2e12784936379083ac54746b09fc8339a1a609
Hannah Wolfe <erisds@gmail.com>
2018-11-29 15:04:21 +0000

    ✨ Add new fix command for packagejson files array
    - make it really easy to add this to any package

--- packages/slimer/commands/fix/packagejson.js
+++ packages/slimer/commands/fix/packagejson.js
@@ -1,18 +1,18 @@
 const cliYo = require('../../lib/cli-yo');
 
 // Internal ID in case we need one.
-exports.id = 'editorconfig';
+exports.id = 'pkgjson';
 
 // The command to run and any params
-exports.flags = 'editorconfig';
+exports.flags = 'packagejson';
 
 // Alias these hard to type commands
-exports.aliases = ['edconf', 'ec'];
+exports.aliases = ['pkgjson', 'pj'];
 
 // Description for the top level command
-exports.desc = 'Rewrite the local .editorconfig';
+exports.desc = 'Update package.json (add a files array)';
 
 // What to do when this command is executed
 exports.run = (argv, cb) => {
-    return cliYo.callGenerator('@tryghost/slimer:editorconfig', argv, cb);
+    return cliYo.callGenerator('@tryghost/slimer:pkgjson', argv, cb);
 };
