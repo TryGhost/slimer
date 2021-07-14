e86b6c6f39066a0a5ff83230eb1de2dcd91a0d15
Daniel Lockyer <hi@daniellockyer.com>
2021-07-14 11:30:53 +0100

    Added c8 test coverage to generated packages
    refs https://github.com/TryGhost/Team/issues/870
    
    - this adds the `c8` dependency to the generated package and prepends
      `c8` to the mocha command so that the module generates coverage info
      from creation

--- packages/generator-slimer/generators/test/index.js
+++ packages/generator-slimer/generators/test/index.js
@@ -1,8 +1,8 @@
 'use strict';
 const Generator = require('../../lib/Generator');
 
-// "test": "NODE_ENV=testing mocha './test/**/*.test.js'",
-const testScript = 'NODE_ENV=testing mocha \'./test/**/*.test.js\'';
+// "test": "NODE_ENV=testing c8 --check-coverage mocha './test/**/*.test.js'",
+const testScript = 'NODE_ENV=testing c8 --check-coverage mocha \'./test/**/*.test.js\'';
 
 const knownOptions = {
     type: {
@@ -65,6 +65,6 @@ module.exports = class extends Generator {
         }
 
         // Test dependencies
-        this.yarnInstall(['mocha', 'should', 'sinon'], options);
+        this.yarnInstall(['c8', 'mocha', 'should', 'sinon'], options);
     }
 };
