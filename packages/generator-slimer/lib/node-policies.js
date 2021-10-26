a519f9c87f23e6b624ce10be874a2d50e262c5e2
Daniel Lockyer <hi@daniellockyer.com>
2021-10-26 19:38:53 +0100

    Added support for Node 16
    refs https://github.com/TryGhost/Toolbox/issues/71
    
    - Node 16 went LTS today so we should now be supporting it
    - also fixes the other Node versions to be current

--- packages/generator-slimer/lib/node-policies.js
+++ packages/generator-slimer/lib/node-policies.js
@@ -1,14 +1,14 @@
 const policies = {
-    lts: [12, 14],
+    lts: [12, 14, 16],
     active: [14, 15, 16],
-    current: [15, 16],
-    lts_active: [14],
-    lts_latest: [14]
+    current: [17],
+    lts_active: [16],
+    lts_latest: [16]
 };
 
 const defaultPolicy = 'lts';
 
-const nodeEngines = '^12.10.0 || ^14.15.0';
+const nodeEngines = '^12.10.0 || ^14.17.0 || ^16.13.0';
 
 module.exports = {
     defaultPolicy,
