f21d2cf265d09b10800679dff40485f07d9b7865
Daniel Lockyer <hi@daniellockyer.com>
2023-01-03 14:31:43 +0100

    Updated Node support ranges
    refs https://github.com/TryGhost/Toolbox/issues/488
    
    - updated ranges for current applicable versions

--- packages/generator-slimer/lib/node-policies.js
+++ packages/generator-slimer/lib/node-policies.js
@@ -1,14 +1,14 @@
 const policies = {
-    lts: [14, 16],
-    active: [14, 15, 16],
-    current: [17],
-    lts_active: [16],
-    lts_latest: [16]
+    lts: [14, 16, 18],
+    active: [16, 18, 20],
+    current: [19],
+    lts_active: [18],
+    lts_latest: [18]
 };
 
 const defaultPolicy = 'lts';
 
-const nodeEngines = '^14.17.0 || ^16.13.0';
+const nodeEngines = '^14.18.0 || ^16.13.0 || ^18.12.1';
 
 module.exports = {
     defaultPolicy,
