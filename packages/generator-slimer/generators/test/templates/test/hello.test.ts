c78c8a44734e319fdfd8f1a60aff23a50619a2e5
Fabien 'egg' O'Carroll <fabien@allou.is>
2023-05-04 12:42:04 -0400

    Fixed default test for TypeScript packages
    We don't have a top level index.js file, so we can instead require the directory which will look up the `main` file defined in `package.json`

--- packages/generator-slimer/generators/test/templates/test/hello.test.ts
+++ packages/generator-slimer/generators/test/templates/test/hello.test.ts
@@ -3,6 +3,6 @@ import assert from 'assert';
 describe('Hello world', function () {
     it('Runs a test', function () {
         // TODO: Write me!
-        assert.ok(require('../index'));
+        assert.ok(require('../'));
     });
 });
