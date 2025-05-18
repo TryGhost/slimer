669ada7593c189cff56c1a2e640d93686b1a1ed2
Hannah Wolfe <github.erisds@gmail.com>
2025-05-18 12:56:12 +0100

    Updated repository in package.json to be an object
    ref https://docs.npmjs.com/cli/v11/configuring-npm/package-json#publishconfig
    - The docs have this field as an object now
    - This updates slimer to use an object, and optionally provide the directory
      when creating packages in a monorepo

--- packages/generator-slimer/generators/node/index.js
+++ packages/generator-slimer/generators/node/index.js
@@ -103,8 +103,15 @@ module.exports = class extends Generator {
     // Add package.json file (super important!)
     _writePackageJson() {
         let repo = this.props.type === 'pkg' ?
-            `${this.props.repo}/tree/main/packages/${this.props.repoName}` :
-            `git@github.com:${this.props.org}/${this.props.repoName}.git`;
+            {
+                type: 'git',
+                url: `git+https://github.com/${this.props.org}/${this.props.repo}.git`,
+                directory: `packages/${this.props.repoName}`
+            } :
+            {
+                type: 'git',
+                url: `git+https://github.com/${this.props.org}/${this.props.repoName}.git`
+            };
 
         this.fs.copyTpl(
             this.templatePath('package.json'),
